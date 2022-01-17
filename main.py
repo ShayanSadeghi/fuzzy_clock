b8d670de0a079c6d8d5871746212ad048c99a325
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-01-18 00:30:42 +0330

    initiate gui

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import time
+from tkinter import *
+from PIL import ImageTk, Image
 from utility import get_fuzzy_time
 
+root = Tk()
+root.title("Fuzzy Clock")
+root.geometry("1024x720")
 fuzzy_time_prime = ""
+
 while True:
     fuzzy_time = get_fuzzy_time()
+    my_label = Label(root, text=fuzzy_time)
+    my_label.pack()
+    root.update_idletasks()
+    root.update()
+
     if fuzzy_time != fuzzy_time_prime:
         fuzzy_time_prime = fuzzy_time
-        print(fuzzy_time)
 
     time.sleep(60)
+    my_label.pack_forget()
