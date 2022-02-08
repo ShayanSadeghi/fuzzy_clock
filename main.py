ff3085aca301e17ac8bdb634fa8c4ddb49ff865f
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-02-08 22:30:01 +0330

    because of halting, we cannot use buttons so it is removed

--- main.py
+++ main.py
@@ -13,18 +13,15 @@ while True:
     plot_fuzzy_minutes()
 
     my_label = Label(root, text=fuzzy_time)
-    btn_quit = Button(root, text="Exit", command=root.quit)
     my_image = ImageTk.PhotoImage(Image.open("graph.png"))
     my_img_label = Label(image=my_image)
 
     my_img_label.pack()
     my_label.pack()
-    btn_quit.pack()
 
     root.update_idletasks()
     root.update()
 
     time.sleep(5)
-    btn_quit.pack_forget()
     my_label.pack_forget()
     my_img_label.pack_forget()
